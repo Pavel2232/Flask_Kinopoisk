dbc72b0530edd9f1f5e59c8b951e955ff89c45e7
Павел <king-he@mail.ru>
2022-10-28 10:11:31 +0300

    refactoring

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import base64
 class Config():
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://movies.db(пользователь):(пароль)@(имя алиас)/(то что передал в постгрес'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pavel2232:10203040x@pg/movie'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PWD_HASH_SALT = b"secret here"
     PWD_HASH_ITERATIONS = 100_000
